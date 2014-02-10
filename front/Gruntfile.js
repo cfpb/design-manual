449958c23747fb17cb5e8936b994a171a1cfc419
danmurphy <daniel.murphy@cfpb.gov>
2014-02-10 15:53:12 -0500

    working on grunt config

--- front/Gruntfile.js
+++ front/Gruntfile.js
@@ -13,18 +13,7 @@ module.exports = function(grunt) {
      * Here's a banner with some template variables.
      * We'll be inserting it at the top of minified assets.
      */
-    banner: 
-      '/*          /$$$$$$          /$$        \n' +
-      '           /$$__  $$        | $$        \n' +
-      '  /$$$$$$$| $$  \\__//$$$$$$ | $$$$$$$  \n' +
-      ' /$$_____/| $$$$   /$$__  $$| $$__  $$  \n' +
-      '| $$      | $$_/  | $$  \\ $$| $$  \\ $$\n' +
-      '| $$      | $$    | $$  | $$| $$  | $$  \n' +
-      '|  $$$$$$$| $$    | $$$$$$$/| $$$$$$$/  \n' +
-      ' \\_______/|__/    | $$____/ |_______/  \n' +
-      '                  | $$                  \n' +
-      '                  | $$                  \n' +
-      '                  |__/                  \n\n' +
+    banner:
       '* <%= pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
@@ -37,12 +26,20 @@ module.exports = function(grunt) {
      * Make sure to add any other CSS libraries/files you'll be using.
      */
     less: {
-      legacy: {
+      main: {
         options: {
-          paths: ['assets/less']
+          paths: ['front/src/less']
         },
         files: {
           'assets/css/<%= pkg.name %>.css': ['<%= banner %>', 'assets/css/normalize.css', 'assets/css/syntax.css', 'assets/css/fonts.css', 'assets/less/style.less']
+        }
+      },
+      ie8: {
+        options: {
+          paths: ['src/static']
+        },
+        files: {
+          'assets/css/design-manual.ie8.css': ['src/less/design-manual.ie8.less']
         }
       }
     },
@@ -57,6 +54,7 @@ module.exports = function(grunt) {
       minify: {
         files: {
           'assets/css/design-manual.min.css': ['assets/css/design-manual.css'],
+          'assets/css/design-manual.ie8.min.css': ['design-manual.ie8.css'],
         }
       }
     },
@@ -73,78 +71,11 @@ module.exports = function(grunt) {
         banner: '<%= banner %>'
       },
       dist: {
-        src: ['assets/js/html5shiv-printshiv.js', 'assets/js/jquery-1.9.1.js', 'assets/js/main.js', '!assets/js/*.min.js'],
+        src: ['front/vendor/html5shiv/dist/html5shiv-printshiv.js', 'front/vendor/jquery/js/jquery.js', 'front/src/js/main.js'],
         dest: 'assets/js/<%= pkg.name %>.min.js'
       }
-    },
-
-    /**
-     * JSHint: https://github.com/gruntjs/grunt-contrib-jshint
-     * 
-     * Validate files with JSHint.
-     * Below are options that conform to idiomatic.js standards.
-     * Feel free to add/remove your favorites: http://www.jshint.com/docs/#options
-     */
-    jshint: {
-      options: {
-        camelcase: true,
-        curly: true,
-        eqeqeq: true,
-        forin: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        quotmark: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true,
-          $: true,
-          Backbone: true,
-          _: true,
-          module: true,
-          Highcharts: true
-        }
-      },
-      all: ['assets/js/main.js']
-    },
-
-    /**
-     * Jasmine: https://github.com/gruntjs/grunt-contrib-jasmine
-     * 
-     * Run jasmine specs headlessly through PhantomJS.
-     * jQuery and Jasmine jQuery is included for your pleasure: https://github.com/velesin/jasmine-jquery
-     */
-    jasmine: {
-      src: '<%= uglify.dist.src %>',
-      options: {
-        specs: 'specs/js/*.js',
-        vendor: [
-          'specs/js/vendor/*.js'
-        ],
-        helpers: [
-          'specs/js/helpers/*.js'
-        ]
-      }
-    },
-
-    /**
-     * Watch: https://github.com/gruntjs/grunt-contrib-watch
-     * 
-     * Run predefined tasks whenever watched file patterns are added, changed or deleted.
-     * Add files to monitor below.
-     */
-    watch: {
-      gruntfile: {
-        files: ['Gruntfile.js', 'static/css/less/*.less', '<%= uglify.dist.src %>', '<%= jasmine.options.specs %>'],
-        tasks: ['default']
-      }
     }
+
   });
 
   /**
@@ -153,11 +84,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-notify');
-
 
   /**
    * Grunt is installed in a sub-directory called "front",  so back out one directory:
@@ -165,13 +91,8 @@ module.exports = function(grunt) {
   grunt.file.setBase('../');
 
   /**
-   * Create task aliases by registering new tasks
-   */
-  grunt.registerTask('test', ['jshint', 'jasmine']);
-
-  /**
    * The 'default' task will run whenever `grunt` is run without specifying a task
    */
-  grunt.registerTask('default', ['test', 'less', 'cssmin', 'uglify']);
+  grunt.registerTask('default', ['less', 'cssmin', 'uglify']);
 
 };
