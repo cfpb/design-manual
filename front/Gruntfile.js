14e317df44516698af8a3c4f1e6f2a768bf16a17
danmurphy <daniel.murphy@cfpb.gov>
2014-02-10 16:31:10 -0500

    updating grunt and src and vendor paths

--- front/Gruntfile.js
+++ front/Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
      */
     banner:
       '* <%= pkg.name %> - v<%= pkg.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
       '* A public domain work of the <%= pkg.author.name %> */\n\n',
 
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
           paths: ['front/src/less']
         },
         files: {
-          'assets/css/<%= pkg.name %>.css': ['<%= banner %>', 'assets/css/normalize.css', 'assets/css/syntax.css', 'assets/css/fonts.css', 'assets/less/style.less']
+          'assets/css/<%= pkg.name %>.css': ['<%= banner %>', 'front/src/less/style.less']
         }
       },
       ie8: {
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
           paths: ['src/static']
         },
         files: {
-          'assets/css/design-manual.ie8.css': ['src/less/design-manual.ie8.less']
+          'assets/css/design-manual.ie8.css': ['front/src/less/design-manual.ie8.less']
         }
       }
     },
@@ -68,14 +68,41 @@ module.exports = function(grunt) {
      */
     uglify: {
       options: {
+        compress: true,
+        mangle: false,
+        beautify: true,
         banner: '<%= banner %>'
       },
-      dist: {
-        src: ['front/vendor/html5shiv/dist/html5shiv-printshiv.js', 'front/vendor/jquery/js/jquery.js', 'front/src/js/main.js'],
-        dest: 'assets/js/<%= pkg.name %>.min.js'
+      vendor: {
+        src: ['front/vendor/jquery/jquery.js'],
+        dest: 'assets/js/vendor.min.js'
+      },
+      html5shiv: {
+        src: ['front/vendor/html5shiv/dist/html5shiv-printshiv.js'],
+        dest: 'assets/js/html5shiv-printshiv.js'
+      },
+      main: {
+        src: ['front/src/js/design-manual.js'],
+        dest: 'assets/js/design-manual.min.js'
+      }
+    },
+
+    /**
+     * Copy: https://github.com/gruntjs/grunt-contrib-copy
+     */
+    copy: {
+      main: {
+        files: [
+          // includes files within path
+          {
+            expand: true,
+            flatten: true,
+            src: ['front/vendor/box-sizing-polyfill/boxsizing.htc'],
+            dest: 'assets/css/',
+            filter: 'isFile'}
+        ]
       }
     }
-
   });
 
   /**
@@ -84,6 +111,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   /**
    * Grunt is installed in a sub-directory called "front",  so back out one directory:
@@ -93,6 +121,6 @@ module.exports = function(grunt) {
   /**
    * The 'default' task will run whenever `grunt` is run without specifying a task
    */
-  grunt.registerTask('default', ['less', 'cssmin', 'uglify']);
+  grunt.registerTask('default', ['less', 'cssmin', 'uglify', 'copy']);
 
 };
