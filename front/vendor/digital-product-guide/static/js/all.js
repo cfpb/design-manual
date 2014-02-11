b4f934500c7b146c16dcf604822b0c66b697481e
danmurphy <daniel.murphy@cfpb.gov>
2014-02-11 11:15:47 -0500

    updated which bower vendor libs get put in github

--- front/vendor/digital-product-guide/static/js/all.js
+++
@@ -1,9 +0,0 @@
----
----
-
-{% include js/jquery-1.9.1.js %}
-{% include js/jquery.cookie.js %}
-{% include js/jquery.append-attr-vals.js %}
-{% include js/jquery.gh-issues.js %}
-{% include js/moment.min.js %}
-{% include js/base.js %}
