70891878dc9b9907270dd240f93946459d7aa770
danmurphy <daniel.murphy@cfpb.gov>
2014-02-12 12:06:58 -0500

    updated footer with code from HMDA docs

--- assets/js/html5shiv-printshiv.js
+++ assets/js/html5shiv-printshiv.js
@@ -2,7 +2,7 @@
  * ==========================================================================
  * Package name: design-manual
  * Version: 0.3.0
- * Last modified: 2014-02-12 11:45:29 AM
+ * Last modified: 2014-02-12 12:06:37 PM
  * URL: https://github.com/cfpb/design-manual/
  * A public domain work of the Consumer Financial Protection Bureau
  * ==========================================================================
