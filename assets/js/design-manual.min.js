0be1aec9bc1a136d6529ec552896654f586f3f5c
danmurphy <daniel.murphy@cfpb.gov>
2014-02-12 15:34:44 -0500

    incorporated most of the HMDA API DOCS CSS

--- assets/js/design-manual.min.js
+++ assets/js/design-manual.min.js
@@ -2,7 +2,7 @@
  * ==========================================================================
  * Package name: design-manual
  * Version: 0.3.0
- * Last modified: 2014-02-12 3:21:08 PM
+ * Last modified: 2014-02-12 3:34:02 PM
  * URL: https://github.com/cfpb/design-manual/
  * A public domain work of the Consumer Financial Protection Bureau
  * ==========================================================================
