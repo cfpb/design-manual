02d4e901e8b52d6e47c385851c2cc796e8aed009
danmurphy <daniel.murphy@cfpb.gov>
2014-02-12 11:46:09 -0500

    updated 2ndary nav with code from HMDA Docs

--- assets/js/design-manual.min.js
+++ assets/js/design-manual.min.js
@@ -2,7 +2,7 @@
  * ==========================================================================
  * Package name: design-manual
  * Version: 0.3.0
- * Last modified: 2014-02-12 11:11:27 AM
+ * Last modified: 2014-02-12 11:45:29 AM
  * URL: https://github.com/cfpb/design-manual/
  * A public domain work of the Consumer Financial Protection Bureau
  * ==========================================================================
