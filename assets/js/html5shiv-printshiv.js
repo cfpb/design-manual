188f3495965fba1c07e9d7c9004b1d8e2aa809b7
danmurphy <daniel.murphy@cfpb.gov>
2014-03-11 13:19:40 -0400

    I forgot to compile the LESS files before committing. Now I changed main nav font size to 18px, fixes https://github.com/cfpb/design-manual/issues/30

--- assets/js/html5shiv-printshiv.js
+++ assets/js/html5shiv-printshiv.js
@@ -2,7 +2,7 @@
  * ==========================================================================
  * Package name: design-manual
  * Version: 0.3.0
- * Last modified: 2014-03-11 1:06:10 PM
+ * Last modified: 2014-03-11 1:19:01 PM
  * URL: https://github.com/cfpb/design-manual/
  * A public domain work of the Consumer Financial Protection Bureau
  * ==========================================================================
