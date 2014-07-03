c885ee447f888db4afe21e1137ed525c7f34d851
davidakennedy <david.kennedy@cfpb.gov>
2014-07-03 11:58:03 -0400

    Add text alternative to external link icon.

--- front/src/js/design-manual.js
+++ front/src/js/design-manual.js
@@ -2,3 +2,11 @@
  * CFPB Design Manual
  * Main JavaScript file
  */
+
+ $( document ).ready(function() {
+    $( ".cf-icon-external-link" ).append( "<span class='external-link-text'> Opens in new window.</span>" );
+    $(".external-link-text").css({
+        "position" : "absolute",
+        "clip" : "rect(1px, 1px, 1px, 1px)"
+    });
+});
