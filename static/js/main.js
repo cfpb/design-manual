dc5a8677ace9fb45d7fd2daa546833d2f4d87dee
Mike Morici <Michael.Morici@cfpb.gov>
2015-01-22 22:28:36 -0500

    Final tweaks needed to convert to updated cf.

--- static/js/main.js
+++ static/js/main.js
@@ -10665,12 +10665,11 @@ jQuery.extend( jQuery.easing,
 
 }(jQuery));
 
-// Count all features included in the test page.
-$('.feature-list').append(
-  '<section class="feature-list_item block block__padded-top block__border-top">' +
-  '<div class="feature-header"><h1 class="feature-header_name">jQuery</h1></div>' +
-  '<p>jQuery is working and counts a total of ' +
-  '<strong>' + $('.feature-list_item').size() + '</strong> ' +
-  'cf-components.</p>' +
-  '</section>'
-);
+/* ==========================================================================
+   JS
+   ========================================================================== */
+
+$(document).ready(function() {
+  'use strict';
+  $('.cf-icon-external-link').append('<span class="u-visually-hidden"> Links to external site.</span>');
+});
